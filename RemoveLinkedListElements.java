a95ac7b7079f963268c4f9e374f141ff1acc83e2
Hasini G <23bq1a1231@vvit.net>
2026-06-20 15:18:18 +0530

    Create RemoveLinkedListElements.java

---
+++ RemoveLinkedListElements.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        while(head!=null && head.val==val){
+            head=head.next;
+        }
+        ListNode prev=null;
+        ListNode curr=head;
+        while(curr!=null){
+            if(curr.val==val){
+              prev.next=curr.next;
+              curr=curr.next;
+            }
+            else{
+            prev=curr;
+            curr=curr.next;
+            }
+        }
+        return head;
+    }
+}
