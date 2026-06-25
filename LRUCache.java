8f1d79329e0e9d599db5d74531b9a8c0cb39fad9
Hasini G <23bq1a1231@vvit.net>
2026-06-25 13:55:28 +0530

    Implement LRU Cache with get and put methods

---
+++ LRUCache.java
@@ -0,0 +1,56 @@
+class LRUCache {
+    class Node {
+        int key, value;
+        Node prev, next;
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    private Node head, tail;
+    private int capacity;
+    private Map<Integer, Node> map;
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    private void addFirst(Node node) {
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        map = new HashMap<>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    public int get(int key) {
+        if (!map.containsKey(key))
+            return -1;
+        Node node = map.get(key);
+        remove(node);
+        addFirst(node);
+        return node.value;
+    }
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node node = map.get(key);
+            node.value = value;
+            remove(node);
+            addFirst(node);
+        } else {
+            if (map.size() == capacity) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node node = new Node(key, value);
+            addFirst(node);
+            map.put(key, node);
+        }
+    }
+}
