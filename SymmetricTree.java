1abebf529a47e24f1c2f357cfd6d7feb1f8adadc
Hasini G <23bq1a1231@vvit.net>
2026-07-30 14:29:02 +0530

    Implement symmetric tree check

---
+++ SymmetricTree.java
@@ -0,0 +1,15 @@
+class SymmetricTree{
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null)
+            return true;
+        if (left == null || right == null)
+            return false;
+        if (left.val != right.val)
+            return false;
+        return isMirror(left.left, right.right) &&
+               isMirror(left.right, right.left);
+    }
+}
