6d0929c9c35ea3f1c7fef69f5f833ba66ee989aa
Hasini G <23bq1a1231@vvit.net>
2026-07-30 15:21:59 +0530

    Implement BalancedBinaryTree class with isBalanced method

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,17 @@
+class BalancedBinaryTree{
+    public boolean isBalanced(TreeNode root) {
+    return height(root)!=-1;
+    }
+    public int height(TreeNode root){
+        if(root==null)
+        return 0;
+        int left=height(root.left);
+        int right=height(root.right);
+        if(left==-1||right==-1)
+        return -1;
+        else if(Math.abs(left-right)>1)
+        return -1;
+        else
+        return Math.max(left,right)+1;
+    }
+}
